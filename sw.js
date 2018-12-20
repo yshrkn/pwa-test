254687acde979264674fe5ff4d1df25fc7dd4cc6
yoshihara <olix-ml@fork.co.jp>
2018-12-20 14:20:07 +0900

    update

--- sw.js
+++ sw.js
@@ -1,33 +1,34 @@
-// キャッシュ名とキャッシュファイルの指定
-const CACHE_NAME = 'pwa-sample-caches';
+// Cache name
+const CACHE_NAME = 'pwa-sample-caches-v1';
+// Cache targets
 const urlsToCache = [
-	'./',
-	'./index.html',
-	'./pages/a.html',
-	'./pages/b.html',
-	'./pages/c.html',
-	'./css/style.css',
-	'./images/a.jpg',
-	'./images/b.jpg',
-	'./images/c.jpg',
+  './',
+  './index.html',
+  './pages/a.html',
+  './pages/b.html',
+  './pages/c.html',
+  './css/style.css',
+  './images/a.jpg',
+  './images/b.jpg',
+  './images/c.jpg',
 ];
 
 self.addEventListener('install', (event) => {
-	event.waitUntil(
-		caches
-			.open(CACHE_NAME)
-			.then((cache) => {
-				return cache.addAll(urlsToCache);
-			})
-	);
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-	event.respondWith(
-		caches
-			.match(event.request)
-			.then((response) => {
-				return response ? response : fetch(event.request);
-			})
-	);
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => {
+        return response ? response : fetch(event.request);
+      })
+  );
 });
